755b90fd15cbd69c4c64e25635afda2c53c6dce0
YoYea <yuttasakcom@gmail.com>
2018-06-06 21:37:24 +0700

    create Blockchain

---
+++ server/services/Blockchain.js
@@ -0,0 +1,16 @@
+const Block = require('./Block')
+
+class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis()]
+  }
+
+  addBlock(data) {
+    const lastBlock = this.chain[this.chain.length - 1]
+    const block = Block.mineBlock(lastBlock, data)
+    this.chain.push(block)
+    return block
+  }
+}
+
+module.exports = Blockchain
